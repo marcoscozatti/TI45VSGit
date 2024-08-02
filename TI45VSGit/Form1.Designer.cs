f95c3722a142d1ac979480f57ac74116b0a5b93d
marcoscozatti <marcoscozatti@gmail.com>
2024-08-02 20:18:18 -0300

    Alteração na solução

--- TI45VSGit/Form1.Designer.cs
+++ TI45VSGit/Form1.Designer.cs
@@ -28,75 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.txValor1 = new System.Windows.Forms.TextBox();
+            this.txValor2 = new System.Windows.Forms.TextBox();
+            this.lblValor1 = new System.Windows.Forms.Label();
+            this.lbValor2 = new System.Windows.Forms.Label();
+            this.lbTotal = new System.Windows.Forms.Label();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.btnSoma = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txValor1
             // 
-            this.textBox1.Location = new System.Drawing.Point(121, 42);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.txValor1.Location = new System.Drawing.Point(121, 42);
+            this.txValor1.Name = "txValor1";
+            this.txValor1.Size = new System.Drawing.Size(100, 20);
+            this.txValor1.TabIndex = 0;
             // 
-            // textBox2
+            // txValor2
             // 
-            this.textBox2.Location = new System.Drawing.Point(121, 79);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
+            this.txValor2.Location = new System.Drawing.Point(121, 79);
+            this.txValor2.Name = "txValor2";
+            this.txValor2.Size = new System.Drawing.Size(100, 20);
+            this.txValor2.TabIndex = 1;
             // 
-            // label1
+            // lblValor1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 48);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Valor 1";
+            this.lblValor1.AutoSize = true;
+            this.lblValor1.Location = new System.Drawing.Point(47, 45);
+            this.lblValor1.Name = "lblValor1";
+            this.lblValor1.Size = new System.Drawing.Size(40, 13);
+            this.lblValor1.TabIndex = 2;
+            this.lblValor1.Text = "Valor 1";
             // 
-            // label2
+            // lbValor2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(47, 82);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Valor 2";
+            this.lbValor2.AutoSize = true;
+            this.lbValor2.Location = new System.Drawing.Point(47, 82);
+            this.lbValor2.Name = "lbValor2";
+            this.lbValor2.Size = new System.Drawing.Size(40, 13);
+            this.lbValor2.TabIndex = 3;
+            this.lbValor2.Text = "Valor 2";
             // 
-            // label3
+            // lbTotal
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(47, 118);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Total";
+            this.lbTotal.AutoSize = true;
+            this.lbTotal.Location = new System.Drawing.Point(47, 118);
+            this.lbTotal.Name = "lbTotal";
+            this.lbTotal.Size = new System.Drawing.Size(31, 13);
+            this.lbTotal.TabIndex = 4;
+            this.lbTotal.Text = "Total";
             // 
-            // label4
+            // lbResultado
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(121, 117);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "label4";
+            this.lbResultado.AutoSize = true;
+            this.lbResultado.Location = new System.Drawing.Point(121, 117);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(16, 13);
+            this.lbResultado.TabIndex = 5;
+            this.lbResultado.Text = "...";
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Location = new System.Drawing.Point(50, 151);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(75, 23);
+            this.btnSoma.TabIndex = 6;
+            this.btnSoma.Text = "Soma";
+            this.btnSoma.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.lbTotal);
+            this.Controls.Add(this.lbValor2);
+            this.Controls.Add(this.lblValor1);
+            this.Controls.Add(this.txValor2);
+            this.Controls.Add(this.txValor1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -106,12 +117,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txValor1;
+        private System.Windows.Forms.TextBox txValor2;
+        private System.Windows.Forms.Label lblValor1;
+        private System.Windows.Forms.Label lbValor2;
+        private System.Windows.Forms.Label lbTotal;
+        private System.Windows.Forms.Label lbResultado;
+        private System.Windows.Forms.Button btnSoma;
     }
 }
 
